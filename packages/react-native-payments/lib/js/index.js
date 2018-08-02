78f5631e6048ef13196b22f3877c167dab8749a0
Kevin Amiranoff <kamiranoff@gmail.com>
2018-08-02 11:14:28 +0100

    uppercase constants

--- packages/react-native-payments/lib/js/index.js
+++ packages/react-native-payments/lib/js/index.js
@@ -3,14 +3,14 @@
 import _PaymentRequest from './PaymentRequest';
 import _PaymentResponse from './PaymentResponse';
 import { PKPaymentButton } from './PKPaymentButton';
-import * as Constants from './constants';
+import * as CONSTANTS from './constants';
 
 const ApplePayButton = PKPaymentButton;
 const PaymentRequest = _PaymentRequest;
 const PaymentResponse = _PaymentResponse;
 
 export {
-  Constants,
+  CONSTANTS,
   ApplePayButton,
   PaymentRequest,
   PaymentResponse,
